20edfd5b6f7471a6067fb05405df7b3109e04a90
vivivek <keviveks@gmail.com>
2014-09-25 20:59:44 +0530

    @viv: CSS and styles in index page

--- index.php
+++ index.php
@@ -16,32 +16,38 @@
 	<body>
 		<div class="container">
 			<div class="header">
+				<div class="header_name">
+					<h1>Shop & Bill</h1>
+				</div>
 			</div>
 			<div class="content">
-				<div id="shop_details">
-					<a id=""><h3></h3></a>
-					<label>Owner : </label>
-					<p></p>
+				<div id="shop_content">
+					<div id="shop_details">
+						<a id=""><h3></h3></a>
+						<label>Owner : </label>
+						<p></p>
+					</div>
 				</div>
-				<form id="shop_registration">
-					<ul>
-						<li><label>Shop Name : </label><input type="text" name="shop" id="shop" /><label for="shop" class="error"></li>
-						<li><label>Owner Name : </label><input type="text" name="owner" id="owner" /></li>
-						<li><label>Shop Address : </label><input type="text" name="address" id="address" /></li>
-						<li><label>Email ID : </label><input type="text" name="email" id="email" /></li>
-						<li><label>Phone No : </label><input type="text" name="phone" id="phone" /></li>
-						<li><label>Mobile No : </label><input type="text" name="mobile" id="mobile" /></li>
-						<li><input type="submit" name="submit" id="shop_submit" value="Save"/>
-					</ul>
-				</form>
-				<div id="status_message"></div>
-				<div id="error_message"></div>
+				<div class="new_shop">
+					<h3>Register New Shop</h3>
+					<form id="shop_registration">
+						<ul>
+							<li><label>Shop Name : </label><input type="text" name="shop" id="shop" /><label for="shop" class="error"></label></li>
+							<li><label>Owner Name : </label><input type="text" name="owner" id="owner" /></li>
+							<li><label>Shop Address : </label><input type="text" name="address" id="address" /></li>
+							<li><label>Email ID : </label><input type="text" name="email" id="email" /></li>
+							<li><label>Phone No : </label><input type="text" name="phone" id="phone" /></li>
+							<li><label>Mobile No : </label><input type="text" name="mobile" id="mobile" /></li>
+							<li><input type="submit" name="submit" id="shop_submit" value="Save"/></li>
+						</ul>
+					</form>
+					<div id="status_message"></div>
+					<div id="error_message"></div>
+				</div>
 			</div>
 			<div class="footer">
-			</div>
-			
+			</div>	
 		</div>
 		<script src="js/index.js"></script>
-		
 	</body>
 </html>
